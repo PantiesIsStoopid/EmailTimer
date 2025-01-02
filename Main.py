5094a8ba2f67f052b5b3ba58b5d81328c340f4a6
nyle <nylewagjiani@gmail.com>
2025-01-02 22:36:17 +0000

    manual running

--- Main.py
+++ Main.py
@@ -1,12 +1,10 @@
 import smtplib
-
 import os
 
 print("hello")
 
 api_key = os.getenv("API_KEY")
 # Use the api_key in your code
-
 
 toaddrs = "NyleWagjiani@Gmail.com"
 fromaddr = "NyleWagjiani@Gmail.com"
@@ -16,7 +14,7 @@ with smtplib.SMTP("smtp.gmail.com", 587) as smtpserver:
     smtpserver.ehlo()
     smtpserver.starttls()
     smtpserver.ehlo()
-    smtpserver.login("NyleWagjiani@Gmail.com", api_key)
+    smtpserver.login(fromaddr, api_key)
 
     for i in range(3):
         smtpserver.sendmail(fromaddr, toaddrs, message)
