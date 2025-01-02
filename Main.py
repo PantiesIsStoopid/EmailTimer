1ab9988e19d7583e41fa9c620b02033bf54fe574
nyle <nylewagjiani@gmail.com>
2025-01-02 22:34:43 +0000

    Add GitHub Actions workflow and initial Python script for sending emails

---
+++ Main.py
@@ -0,0 +1,23 @@
+import smtplib
+
+import os
+
+print("hello")
+
+api_key = os.getenv("API_KEY")
+# Use the api_key in your code
+
+
+toaddrs = "NyleWagjiani@Gmail.com"
+fromaddr = "NyleWagjiani@Gmail.com"
+message = "hello"
+
+with smtplib.SMTP("smtp.gmail.com", 587) as smtpserver:
+    smtpserver.ehlo()
+    smtpserver.starttls()
+    smtpserver.ehlo()
+    smtpserver.login("NyleWagjiani@Gmail.com", api_key)
+
+    for i in range(3):
+        smtpserver.sendmail(fromaddr, toaddrs, message)
+        print(f"Email sent {i+1}")
